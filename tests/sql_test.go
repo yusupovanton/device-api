cc16df91ff535a1bf1f20e79d2bcba5c510f7cc8
yusupovanton <antonyusupov@outlook.com>
2022-10-30 23:52:18 +0300

    QAHW5: SQL function with test suites

--- tests/sql_test.go
+++ tests/sql_test.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"database/sql"
+	"fmt"
 	"testing"
 
 	// "log"
@@ -13,12 +14,19 @@ import (
 	"github.com/ozontech/allure-go/pkg/framework/provider"
 	// "github.com/ozontech/allure-go/pkg/framework/runner"
 	_ "github.com/lib/pq"
-	"github.com/ozontech/allure-go/pkg/framework/runner"
 	act_device_api "gitlab.ozon.dev/qa/classroom-4/act-device-api/pkg/act-device-api/gitlab.ozon.dev/qa/classroom-4/act-device-api/pkg/act-device-api"
 	"google.golang.org/grpc"
 )
 
 var ctx = context.Background()
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "docker"
+	password = "docker"
+	dbname   = "postgres"
+)
 
 type BeforeAfterDemoSuite struct {
 	suite.Suite
@@ -26,56 +34,68 @@ type BeforeAfterDemoSuite struct {
 
 func (s *BeforeAfterDemoSuite) BeforeEach(t provider.T) {
 	t.NewStep("Before Test Step")
-
-	var connStr = "psql -h localhost -p 5432 -U docker -d act_device_api"
-	sql.Open("postgres", connStr)
+	fmt.Println("Test started")
 
 }
 
 func (s *BeforeAfterDemoSuite) AfterEach(t provider.T) {
 	t.NewStep("After Test Step")
+	fmt.Println("Test completed")
 }
 
-func (s *BeforeAfterDemoSuite) TestSQL(t provider.T) {
+func (s *BeforeAfterDemoSuite) TestSQLUpdateDeviceV1(t provider.T) {
 
-}
-
-func TestBeforesAfters(t *testing.T) {
-	t.Parallel()
-	suite.RunSuite(t, new(BeforeAfterDemoSuite))
 	// В тесте UpdateDevice убедиться что изменения проливаются в БД, а не вернулись из кеша;
+
+	t.Title("SQL tests of the update function")
+	t.Description("Creates the device, updates it and then looks for the updates in the postgres service")
 	ctx := context.Background()
 	addr := "localhost:8082"
-	runner.Run(t, "Test_grpc_UpdateDeviceV1", func(t provider.T) {
-		t.WithNewStep("Testing UpdateDeviceV1", func(sCtx provider.StepCtx) {
 
-			sCtx.NewStep("Dialing the service")
-			conn, err := grpc.Dial(addr, grpc.WithInsecure())
-			sCtx.Require().NoError(err)
-			defer conn.Close()
+	t.NewStep("Dialing the service")
+	conn, err := grpc.Dial(addr, grpc.WithInsecure())
+	t.Require().NoError(err)
+	defer conn.Close()
 
-			sCtx.NewStep("Creating the device")
-			client := act_device_api.NewActDeviceApiServiceClient(conn)
-			platformEx := "MegaOS"
-			const userIdEx uint64 = 991991
-			req := &act_device_api.CreateDeviceV1Request{Platform: platformEx, UserId: userIdEx}
-			res, err := client.CreateDeviceV1(ctx, req)
-			sCtx.Require().NoError(err)
-			sCtx.Require().NotNil(res.DeviceId)
+	t.NewStep("Creating the device")
+	client := act_device_api.NewActDeviceApiServiceClient(conn)
+	platformEx := "MegaOS"
+	const userIdEx uint64 = 991991
+	req := &act_device_api.CreateDeviceV1Request{Platform: platformEx, UserId: userIdEx}
+	res, err := client.CreateDeviceV1(ctx, req)
+	t.Require().NoError(err)
+	t.Require().NotNil(res.DeviceId)
 
-			sCtx.NewStep("Updating the device")
-			newPlatformEx, newUserIdEx := "Ios", uint64(100101)
-			reqUpd := &act_device_api.UpdateDeviceV1Request{Platform: newPlatformEx, UserId: newUserIdEx, DeviceId: res.DeviceId}
-			resUpd, err := client.UpdateDeviceV1(ctx, reqUpd)
-			sCtx.Require().NoError(err)
-			sCtx.Require().Equal(true, resUpd.Success)
+	t.NewStep("Updating the device")
+	newPlatformEx, newUserIdEx := "Ios", uint64(100101)
+	reqUpd := &act_device_api.UpdateDeviceV1Request{Platform: newPlatformEx, UserId: newUserIdEx, DeviceId: res.DeviceId}
+	resUpd, err := client.UpdateDeviceV1(ctx, reqUpd)
+	t.Require().NoError(err)
+	t.Require().Equal(true, resUpd.Success)
 
-			sCtx.NewStep("Describing the device")
-			reqDesc := &act_device_api.DescribeDeviceV1Request{DeviceId: res.DeviceId}
-			resDesc, err := client.DescribeDeviceV1(ctx, reqDesc)
-			sCtx.Require().NoError(err)
-			sCtx.Require().Equal(newPlatformEx, resDesc.Value.Platform)
-			sCtx.Require().Equal(newUserIdEx, resDesc.Value.UserId)
-		})
-	})
+	t.NewStep("Describing the device")
+	reqDesc := &act_device_api.DescribeDeviceV1Request{DeviceId: res.DeviceId}
+	resDesc, err := client.DescribeDeviceV1(ctx, reqDesc)
+	t.Require().NoError(err)
+	t.Require().Equal(newPlatformEx, resDesc.Value.Platform)
+	t.Require().Equal(newUserIdEx, resDesc.Value.UserId)
+
+	t.NewStep("SQL check")
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	t.Require().NoError(err)
+	queryUpdatedDevice := fmt.Sprintf("SELECT user_id, platform FROM devices WHERE id = %d;", res.DeviceId)
+	row := db.QueryRow(queryUpdatedDevice)
+	device := new(Device)
+	err = row.Scan(&device.UserId, &device.Platform)
+	t.Require().NoError(err)
+	t.Require().Equal(newUserIdEx, device.UserId)
+	t.Require().Equal(newPlatformEx, device.Platform)
+
+}
+
+func TestSQLUpdateDevice(t *testing.T) {
+	t.Parallel()
+	suite.RunSuite(t, new(BeforeAfterDemoSuite))
+
 }
